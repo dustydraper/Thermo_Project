e8c8f7dfa39ba1699c125cfea939104af9122546
dustin <dustin.draper@tum.de>
2015-06-29 11:12:21 +0200

    "debug in parallel"

--- solveStream.m
+++ solveStream.m
@@ -64,10 +64,10 @@ for ii = 1:dimY
         for jj = 2:(dimX-1)
             
             A(index(ii,jj),index(ii,jj)) = -2/(delta_x^2)-2/(delta_y^2);
-            A(index(ii,jj),index(ii - 1,jj) ) = 1/(delta_x^2);
-            A(index(ii,jj),index(ii + 1,jj) ) = 1/(delta_x^2);
-            A(index(ii,jj),index(ii,jj+1) ) = 1/(delta_y^2);
-            A(index(ii,jj),index(ii,jj-1) ) = 1/(delta_y^2);
+            A(index(ii,jj),index(ii - 1,jj) ) = 1/(delta_y^2);
+            A(index(ii,jj),index(ii + 1,jj) ) = 1/(delta_y^2);
+            A(index(ii,jj),index(ii,jj+1) ) = 1/(delta_x^2);
+            A(index(ii,jj),index(ii,jj-1) ) = 1/(delta_x^2);
 
         end
     end
